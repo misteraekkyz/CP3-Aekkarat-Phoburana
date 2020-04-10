5eeb4ed590c692b6c6026859d67d9f25705096d7
misteraekkyz <55557651+misteraekkyz@users.noreply.github.com>
2020-04-10 10:29:09 +0700

    Exercise : 1
    Exercise test BorntoDev

---
+++ Assignments/Exercise4_Aekkarat_P.py
@@ -0,0 +1,11 @@
+YS = "----- Your Score -----"
+FE = "Foundation English                    :   70.0"
+GB = "General Business                      :   85.0"
+IC = "Introduction to Computer Systems      :   65.0"
+CP = "Computer Programming                  :   100.0"
+
+print(YS)
+print(FE)
+print(GB)
+print(IC)
+print(CP)
