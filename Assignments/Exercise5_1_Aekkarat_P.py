c0b35f5b381378682af77a9b44b89b297832c0c3
misteraekkyz <55557651+misteraekkyz@users.noreply.github.com>
2020-04-10 12:55:43 +0700

    Exercise : 2

---
+++ Assignments/Exercise5_1_Aekkarat_P.py
@@ -0,0 +1,7 @@
+number1 = int(input("input number1 : "))
+number2 = int(input("input number2 : "))
+
+print(number1,"+",number2,"=",number1+number2)
+print(number1,"-",number2,"=",number1-number2)
+print(number1,"*",number2,"=",number1*number2)
+print(number1,"/",number2,"=",int(number1/number2))
