74962345ec2e539c62a8c848cb4b64773b6959a1
misteraekkyz <55557651+misteraekkyz@users.noreply.github.com>
2020-04-12 02:28:18 +0700

    Exercise : 6

---
+++ Assignments/Lecture50_Aekkarat_P.py
@@ -0,0 +1,13 @@
+def addnumberPlus(x,y):
+    print(x+y)
+def addnumberMinus(a,b):
+    print(a-b)
+def addnumberMultiplied(c,v):
+    print(c*v)
+def addnumberDivide(n,m):
+    print(n/m)
+addnumberPlus(100,50)
+addnumberMinus(100,50)
+addnumberMultiplied(100,50)
+addnumberDivide(100,50)
+
